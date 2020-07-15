2462d690af22e8918673acdef4f5c9e86f5cee40
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2020-07-16 00:19:45 +0200

    beuatify some methods

--- src/repository/Teams.kt
+++ src/repository/Teams.kt
@@ -8,33 +8,47 @@ class Teams {
     companion object {
         private const val spreadsheetId = "1E9YtPFw1VGnkOW3RllXe9Kvhcmgu1xmQU15Y8mSuy04"
         private const val range = "Planning orizzonte!A4:C"
-        private val BLACKLIST = listOf("Altro", "Governance")
+        private val BLACKLIST = listOf(
+            "Altro",
+            "Governance",
+            "Aggiornamenti WP",
+            "Aggiorn. EZ Fiaso"
+        )
     }
 
-    private fun getPeople(team: String, values: List<List<Any>>): List<String> {
-        return values
-            .filter { l -> l.size == 3 }
+    private val isValidRow = { l: List<Any> -> l.size == 3 }
+
+    private val getTeamNameFromRow = { l: List<Any> -> l[0] as String }
+
+    private val isValidTeamName = { teamName: String -> teamName.isNotEmpty() && !BLACKLIST.contains(teamName) }
+
+    private fun getPeopleForTeam(team: String, rows: List<List<Any>>): List<String> {
+        return rows
+            .filter(isValidRow)
             .map { l -> Pair(l[0] as String, l[2] as String) }
             .filter { p -> p.first.equals(team) }
             .map { p -> p.second }
     }
 
+    private val teamNameToTeam = { rows: List<List<Any>> ->
+        { teamName: String ->
+            Team(
+                name = teamName,
+                people = getPeopleForTeam(teamName, rows)
+            )
+        }
+    }
+
     fun list(): List<Team> {
-        val values = sheets.getValues(
+        val rows = sheets.getValues(
             spreadsheetId,
             range
         )
-        return values
-            .filter { l -> l.size == 3 }
-            .map { l -> l[0] as String }
-            .filter { teamName -> teamName.isNotEmpty() }
-            .filter { teamName -> !BLACKLIST.contains(teamName) }
+        return rows
+            .filter(isValidRow)
+            .map(getTeamNameFromRow)
+            .filter(isValidTeamName)
             .distinct()
-            .map { team ->
-                Team(
-                    name = team,
-                    people = getPeople(team, values)
-                )
-            }
+            .map(teamNameToTeam(rows))
     }
 }
