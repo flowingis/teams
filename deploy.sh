dab5c1d2820875085e8f23a379ec97355b84a580
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2020-08-20 17:33:57 +0200

    using beanstalk to deploy app

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,2 @@
-git branch -D deploy &>/dev/null
-git checkout -b deploy
-mv .gitignore_deploy .gitignore
-cp resources/service.json resources/_service.json
-cp resources/credentials.json resources/_credentials.json
-git add resources/service.json
-git add resources/credentials.json
-git commit -am "deploy"
-git push heroku deploy:master --force
-git checkout master
-mv resources/_service.json resources/service.json
-mv resources/_credentials.json resources/credentials.json
+mvn package
+eb deploy -v
